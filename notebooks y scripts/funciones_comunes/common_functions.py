79034588e36bd8159da39fe2eb394d256ebfd7a9
Yane, Ian <ian.00@hotmail.com>
2024-05-30 17:37:22 -0300

    se pule card de recomendacion y se agrega audio y card de recomendacion al diccionario que se devolvera como ouput

--- notebooks y scripts/funciones_comunes/common_functions.py
+++ notebooks y scripts/funciones_comunes/common_functions.py
@@ -1,3 +1,21 @@
+import re
+
 def limpiar_signo_peso(texto):
-    texto = texto.replace('$', 'ars')
+    try:
+        texto = texto.replace('$', '')  # Eliminar el símbolo de dólar
+        texto = texto.replace('.', '')  # Eliminar los puntos de separación de miles
+        texto = texto.replace(',', '.')  # Reemplazar la coma decimal por un punto
+        precio = int(float(texto))
+    except:
+        precio = None
+    return precio # Convertir a número flotante y luego a entero
+
+def redondear_numeros(texto):
+    # Buscar todos los números con decimales
+    numeros = re.findall(r'\d+\.\d+', texto)
+    for numero in numeros:
+        # Convertir a entero (redondear eliminando decimales)
+        entero = str(int(float(numero)))
+        # Reemplazar en el texto
+        texto = texto.replace(numero, entero)
     return texto
