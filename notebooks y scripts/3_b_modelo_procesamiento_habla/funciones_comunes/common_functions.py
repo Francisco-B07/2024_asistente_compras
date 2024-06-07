49e98f93c265ee5a6da2fd6d93633aa2ff989d07
Yane, Ian <ian.00@hotmail.com>
2024-06-06 21:52:28 -0300

    Se agrega etapa de evaluacion

--- notebooks y scripts/3_b_modelo_procesamiento_habla/funciones_comunes/common_functions.py
+++ notebooks y scripts/3_b_modelo_procesamiento_habla/funciones_comunes/common_functions.py
@@ -1,4 +1,43 @@
+# Create and activate virtual env for speech processing stage
 import re
+import spacy
+from spacy.training import Example
+from spacy.util import minibatch
+from spacy import displacy
+
+# Función encargada de procesar texto
+def procesar_texto(texto, graph=None, nlp = None):
+    texto = texto.lower()  # convierte el texto a minúsculas
+    texto = re.sub(r'[,\.;:!\-*#@$!+_%^&`~]', '', texto)  # Elimina los caracteres especiales
+    texto = re.sub(r'\s+', ' ', texto)  # Elimina los espacios en blanco adicionales
+
+    thresgold = 1  # se establece el umbral para la longitud mínima de las palabras
+
+    doc = nlp(texto)  # Se pre-procesa el texto utilizando la clase pre-etrenada de spaCy
+
+    if graph == True:
+        displacy.render(doc, style='dep', options={'distance':100})
+
+    # Se obtienen los tokens, lemas y etiquetas de parte del discurso para las palabras filtradas
+    tokens = [token.text for token in doc]
+    # se definen la lista de palabras que no aportan
+    stopwords = spacy.lang.es.stop_words.STOP_WORDS
+    # se filtran los tokens por medio de diversas condiciones
+    filtered_tokens = [
+        token.text for token in doc # se recorre cada token de la entrada
+        if token.text not in stopwords # si comprueba que el token este fuera de un stopword
+        and token.pos_ in ('NOUN', 'ADJ', 'ADP') # se comprueba que la etiqueta gramatical del token sea sustantivo, adjetivo o proposicion
+        and len(token.text) > thresgold # Se comprueba que la longitud del token sea mayor al umbral previamente establecido
+        ]
+    lemmas = [token.lemma_ for token in doc if token.text in filtered_tokens]
+    pos_tags = [(token.text, token.pos_) for token in doc if token.text in filtered_tokens]
+    
+    return {
+        "tokens": tokens,
+        "lemmas": lemmas,
+        "filtered_tokens": filtered_tokens,
+        "pos_tags": pos_tags,
+    }
 
 def limpiar_signo_peso(texto):
     try:
